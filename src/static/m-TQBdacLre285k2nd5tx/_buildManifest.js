9dcd169150c4ec6a629abb2a115478abf386d9d6
Arogunb2021 <blessingarogunbo@gmail.com>
2024-02-25 12:17:06 +0100

    low

---
+++ src/static/m-TQBdacLre285k2nd5tx/_buildManifest.js
@@ -0,0 +1,11 @@
+self.__BUILD_MANIFEST = {
+    __rewrites: {
+        afterFiles: [],
+        beforeFiles: [],
+        fallback: []
+    },
+    "/": ["static/chunks/pages/index-5283fc15be12745a.js"],
+    "/_error": ["static/chunks/pages/_error-5a00309fd5f4b49e.js"],
+    "/quests": ["static/chunks/pages/quests-b665cb46c1241dfa.js"],
+    sortedPages: ["/", "/_app", "/_error", "/quests"]
+}, self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
